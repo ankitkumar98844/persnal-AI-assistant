802b65096956325c15b992b789a8d66df0e21271
ankitkumar98844 <ankitkumar988449@gmail.com>
2026-07-08 14:42:41 +0530

    MusicLibrary add

---
+++ musiclibrary.py
@@ -0,0 +1,10 @@
+music ={
+    "dad":"https://www.youtube.com/watch?v=Rj_G8TZhhyo&pp=ygUSZ3VsemFhciBjaGhhbml3YWxh",
+    "jug":"https://www.youtube.com/watch?v=GjYemNgJWg0&pp=ygUSZ3VsemFhciBjaGhhbml3YWxh",
+    "tandav":"https://www.youtube.com/watch?v=sR6Ir2aG24Q&pp=ygUSZ3VsemFhciBjaGhhbml3YWxh",
+    "filter":"https://www.youtube.com/watch?v=ztNkwPMCfqc&pp=ygUSZ3VsemFhciBjaGhhbml3YWxh",
+    "Rakshas":"https://www.youtube.com/watch?v=LaSGZGzMw5Q&pp=ygUSZ3VsemFhciBjaGhhbml3YWxh",
+    "godfather":"https://www.youtube.com/watch?v=X48UDSJIDl8&pp=ygUSZ3VsemFhciBjaGhhbml3YWxh0gcJCfsJAYcqIYzv",
+
+
+}
